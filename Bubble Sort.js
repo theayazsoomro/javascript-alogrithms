646d161253fe126b44e0e908ed526f8f665ae976
Ayaz Soomro <theayazsoomro@gmail.com>
2023-02-09 15:31:29 +0500

    Bubble Sort Algorithm

---
+++ Bubble Sort.js
@@ -0,0 +1,27 @@
+const arr = [9,3,2,4,1,5,8,6,7];
+const len = arr.length;
+
+function bubbleSort(arr, len)
+{
+    // Implementing a bubble sort algorithm to sort the unsorted data
+    for(let i = 0; i < len; i++)
+    {
+        for(let j = 0; j < len - i - 1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                let temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Display sorted data
+    for(let i = 0; i < len; i++)
+    {
+        console.log(arr[i]);
+    }
+}
+// Calling a function
+bubbleSort(arr, len);
